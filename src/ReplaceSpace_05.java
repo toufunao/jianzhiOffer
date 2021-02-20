dede74da12786dcee39f4906b88fb94cea8700f4
toufunao <wurjpersonal@126.com>
2021-02-20 19:40:57 +0800

    Initial commit

---
+++ src/ReplaceSpace_05.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ */
+
+public class ReplaceSpace_05 {
+    public String replaceSpace(String s) {
+        StringBuilder stringBuilder=new StringBuilder();
+        for (int i=0;i<s.length();i++) {
+            if (s.charAt(i)==' '){
+                stringBuilder.append("%20");
+            }else
+                stringBuilder.append(s.charAt(i));
+        }
+        return stringBuilder.toString();
+    }
+}
