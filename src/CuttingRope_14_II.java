45588cb9ee6307d55ea4ce151e8b37cd927b0e23
toufunao <wurjpersonal@126.com>
2021-02-23 12:22:46 +0800

    12,13,14,15,16题

---
+++ src/CuttingRope_14_II.java
@@ -0,0 +1,16 @@
+public class CuttingRope_14_II {
+    //数学推导
+    public int cuttingRope(int n) {
+        if(n<=3)
+            return n-1;
+        if (n==4)
+            return 4;
+        long ans=1;
+        while (n>4){
+            ans*=3;
+            ans%=1000000007;
+            n-=3;
+        }
+        return (int) (ans*n%1000000007);
+    }
+}
---
+++ src/PathInMatrix_12.java
@@ -0,0 +1,33 @@
+public class PathInMatrix_12 {
+    public boolean exist(char[][] board, String word) {
+        int row=board.length;
+        int col=board[0].length;
+        char[] words=word.toCharArray();
+
+        int[][] visited=new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (dfs(board,words,visited,i,j,0))
+                    return true;
+            }
+        }
+        return false;
+
+    }
+
+    private boolean dfs(char[][] board, char[] words, int[][] visited, int i, int j, int index) {
+        if (i>=board.length || i<0||j>=board[0].length||j<0|| board[i][j]!=words[index]||visited[i][j]==1)
+            return false;
+        if (index==words.length-1)
+            return true;
+
+        visited[i][j]=1;
+        boolean ans=dfs(board,words,visited,i+1,j,index+1) || dfs(board,words,visited,i-1,j,index+1)||
+                dfs(board,words,visited,i,j+1,index+1) || dfs(board,words,visited,i,j-1,index+1);
+
+        visited[i][j]=0;
+
+        return ans;
+    }
+}
